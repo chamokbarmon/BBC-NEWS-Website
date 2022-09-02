a46a4382a1df3d86fb510ed7d1b73354506ff709
chamokBarmon <chamokbarmon123@gmail.com>
2022-09-02 23:59:56 +0600

    third

--- index.js
+++ index.js
@@ -33,13 +33,24 @@ const showCatagory = show =>{
     <div class="card mb-3" style="max-width:100%;">
     <div class="row g-0">
       <div class="col-md-4">
-        <img src="${show.image_url}" class="img-fluid rounded-start" alt="...">
+        <img src="${show.image_url}" class="img-fluid h-100 rounded-start" alt="...">
       </div>
       <div class="col-md-8">
         <div class="card-body">
           <h5 class="card-title">${show.title}</h5>
           <p class="card-text text-truncate" >${show.details.slice(0,500)}</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          <div class="d-flex">
+          <img style="height:40px; width:40px" class=" m-3 img-fluid rounded-circle" src="${show.author.img}">
+          <div>
+          <span class="d-block mt-2" >${show.author.name}</span> 
+          <span class="d-block">${show.author.published_date}</span>
+          </div>
+          
+          </div>
+          
+      
+         
+        
         </div>
       </div>
     </div>
